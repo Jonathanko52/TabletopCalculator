25d7ace2a8407c909baf79752dd07021226e38b3
Jonathanko52 <jonathan.ko523@gmail.com>
2025-04-05 19:11:57 -0700

    PArsing work

--- Test.py
+++ Test.py
@@ -15,9 +15,33 @@ with open(route) as xml_file:
 # for key in data_dict["catalogue"]["sharedSelectionEntries"]:
 #     print("KEY", key)
 
-count = 0
+nested = False
 for key in data_dict["catalogue"]["sharedSelectionEntries"]["selectionEntry"]:
-    count += 1
-    print("_______________LINE BREAK_____________")
-    print(key)
-print(count)
+    if(key["@type"] == "model" or key["@type"] == "unit"):
+        print("_______________LINE BREAK_____________")
+        print("NAME:",key["@name"])
+        if "selectionEntryGroups" in key:
+            allEntryGroups = key["selectionEntryGroups"]['selectionEntryGroup']["selectionEntries"]["selectionEntry"]
+            # if
+
+            for value in allEntryGroups:
+                print("VALUE TYPE:", type(value))
+                if(type(value) is dict):
+                    print("CASE 1:")
+                    print(value)
+                if(type(value) is str):
+                    print("CASE 2:")
+                    print(value)
+
+
+
+        # if "selectionEntries" in key:
+            # allEntries = key["selectionEntries"]['selectionEntry']
+            # print("_______________SELECTION ENTRIES_____________")
+            # print("TYPE:",len(allEntries))
+            # for value in  allEntries:
+            #     print(value)
+
+     
+
+
