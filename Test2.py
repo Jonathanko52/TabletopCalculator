f792c935e75e147bce969e29451dff95f02f0c0f
Jonathanko52 <jonathan.ko523@gmail.com>
2025-04-06 16:52:24 -0700

    Parsing

--- Test2.py
+++ Test2.py
@@ -1,5 +1,6 @@
 import json
 import xmltodict
+import re 
 
 codexName = "Imperium - Adeptus Custodes"
 route = ('./40k/' + codexName + '.cat')
@@ -11,33 +12,81 @@ def parseUnitModelToDictionary(input):
     return returnDict
 
 
-def findUnitModel(input, outputList):
+def findModel(input, outputList):
+    if input is None:
+      return
+    
+    if type(input) == dict:
+        for keys in input:
+            if keys == "@type":
+                if(input["@type"]== "model"):
+                    outputList.append(input)
+            else:
+                findModel(input[keys],outputList)
+    if type(input) == list:
+        for values in range(len(input)):
+                findModel(input[values],outputList)
+
+
+
+def findUnit(input, outputList):
     if input is None:
       return
     
     if type(input) == dict:
         for keys in input:
             if "@typeName" in keys:
-                if(input["@typeName"]== "Model" or input["@typeName"]== "Unit"):
-                    # print(input)
+                if(input["@typeName"]== "Unit"):
                     outputList.append(parseUnitModelToDictionary(input))
             else:
-                findUnitModel(input[keys],outputList)
+                findUnit(input[keys],outputList)
     if type(input) == list:
         for values in range(len(input)):
-                findUnitModel(input[values],outputList)
+                findUnit(input[values],outputList)
         
-
+def findWeapon(input, outputList):
+    if input is None:
+      return
+    
+    if type(input) == dict:
+        for keys in input:
+            if "@typeName" in keys:
+                if(input["@typeName"]== "Melee Weapons" or input["@typeName"]== "Ranged Weapons"):
+                    outputList.append(parseUnitModelToDictionary(input))
+            else:
+                findWeapon(input[keys],outputList)
+    if type(input) == list:
+        for values in range(len(input)):
+                findWeapon(input[values],outputList)
 
 with open(route) as xml_file:
     data_dict = xmltodict.parse(xml_file.read())
 
 
-result = []
-findUnitModel(data_dict,result)
-for value in result:
+result1 = []
+
+result2 = []
+
+result3 = []
+
+findModel(data_dict, result1)
+
+findUnit(result1, result2)
+
+findWeapon(result1, result3)
+
+# print(result1[0])
+
+for value in result1:
+    print(value["@name"])
+print("_________________________________")
+for value in result2:
     print(value["unitName"])
-    print(value["characteristics"])
+print("_________________________________")
+for value in result3:
+    print(value["unitName"])
 
-
+print(len(result1))
+print(len(result2))
+print(len(result3))
 
