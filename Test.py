28e804929d62302669ef4e4d8e697303349c124c
Jonathanko52 <jonathan.ko523@gmail.com>
2025-04-05 19:52:56 -0700

    Breakthough: Found weapons profiles for single loadout units

--- Test.py
+++ Test.py
@@ -17,21 +17,39 @@ with open(route) as xml_file:
 
 nested = False
 for key in data_dict["catalogue"]["sharedSelectionEntries"]["selectionEntry"]:
+    # Identifies a unit entry
     if(key["@type"] == "model" or key["@type"] == "unit"):
         print("_______________LINE BREAK_____________")
         print("NAME:",key["@name"])
+        # Weapons Profiles
         if "selectionEntryGroups" in key:
             allEntryGroups = key["selectionEntryGroups"]['selectionEntryGroup']["selectionEntries"]["selectionEntry"]
-            # if
+            if "selectionEntries" in allEntryGroups:
+                # print("CASE 2: Single profile Datasheets")
+                # print(allEntryGroups["selectionEntries"]['selectionEntry'])
+                for value in allEntryGroups["selectionEntries"]['selectionEntry']:
+                    if type(value) is dict:
+                            unitProfile = value["profiles"]["profile"]
+                            characteristics = unitProfile["characteristics"]['characteristic']
+                            print("NAME: ", unitProfile["@name"])
+                            print("CHARACTERISTICS: ", characteristics[0])
+                            print("CHARACTERISTICS: ", characteristics[1])
+                            print("CHARACTERISTICS: ", characteristics[2])
+                            print("CHARACTERISTICS: ", characteristics[3])
+                            print("CHARACTERISTICS: ", characteristics[4])
+                            print("CHARACTERISTICS: ", characteristics[5])
+                            print("CHARACTERISTICS: ", characteristics[6])
+                            print("TYPE: ", unitProfile["@typeName"])
+                            
+                            # print("Characteristics: ", value["characteristics"])
+                        # for innerkeys in value:
+                        #     print("ENTRY: ", innerkeys)
+            # else:
+            #     for value in allEntryGroups:
+            #         if(type(value) is dict):
+            #             # print("CASE 1: Multiple weapon loadout datasheets")
+            #             print(value)
 
-            for value in allEntryGroups:
-                print("VALUE TYPE:", type(value))
-                if(type(value) is dict):
-                    print("CASE 1:")
-                    print(value)
-                if(type(value) is str):
-                    print("CASE 2:")
-                    print(value)
 
 
 
